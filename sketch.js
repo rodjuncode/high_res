20fec67386de8691cdddaaeeed277b7351c2ae34
Rodrigo Junqueira <oi.rodjun@gmail.com>
2023-11-07 17:03:58 -0300

    Resultados dos experimentos com o scaler

--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 function setup() {
   
-  // createAdaptiveCanvas(400, 400);
-  // scaler.scaleCanvasTo(20);
+  createAdaptiveCanvas(400, 400);
+  scaler.scaleCanvasTo(20);
 
   // createAdaptiveCanvas(0.4, 0.4, false);
   // scaler.scaleCanvasTo(20000);
 
-  createAdaptiveCanvas(16000, 16000, false);
-  scaler.scaleCanvasTo(0.5);
+  // createAdaptiveCanvas(16000, 16000, false);
+  // scaler.scaleCanvasTo(0.5);
 
+
+  // ##### Observações
 
   // Maximum canvas size (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas)
   // The exact maximum size of a <canvas> element depends on the browser and environment. While in most cases the maximum dimensions exceed 10,000 x 10,000 pixels, notably iOS devices limit the canvas size to only 4,096 x 4,096 pixels. See canvas size limits in different browsers and devices (2021).
+
+  // Verificar resolução do Mac. 72dpi? Aparentemente Windows é 32dpi. Checar.
 
 
 }
