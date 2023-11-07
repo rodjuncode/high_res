955f409d8e3b5f22b370fb925e2ea83f78820316
Rodrigo Junqueira <oi.rodjun@gmail.com>
2023-11-07 17:20:09 -0300

    Atualização nas referências

--- sketch.js
+++ sketch.js
@@ -13,13 +13,23 @@ function setup() {
   // scaler.scaleCanvasTo(0.5);
 
 
-  // #####Observações
+  // ##### Observações
 
+  // A
+  // Bresenham’s Line Drawing Algorithm 
+  // https://www.youtube.com/watch?v=8gIhNSAXYcQ
+  // + Anti-Aliasing (Xiaolin Wu's Line Algorithm)
+
+  // B
   // Maximum canvas size (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas)
   // The exact maximum size of a <canvas> element depends on the browser and environment. While in most cases the maximum dimensions exceed 10,000 x 10,000 pixels, notably iOS devices limit the canvas size to only 4,096 x 4,096 pixels. See canvas size limits in different browsers and devices (2021).
 
+  // C
   // Verificar resolução do Mac. 72dpi? Aparentemente Windows é 32dpi. Checar.
 
+  // D
+  // Teste para tamanho máximo do <canvas>
+  // https://on690.codesandbox.io/
 
 }
 
