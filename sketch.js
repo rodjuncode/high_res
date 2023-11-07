2df771c21408efbdc1f262b491152ad368bc54f1
Rodrigo Junqueira <oi.rodjun@gmail.com>
2023-11-07 17:08:11 -0300

    Adição de um novo caso de teste

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-  
-  createAdaptiveCanvas(400, 400);
-  scaler.scaleCanvasTo(20);
+
+  createAdaptiveCanvas(8000, 8000, false);
+  // scaler.scaleCanvasTo(1); // <-- não é necessário
+
+  // createAdaptiveCanvas(400, 400, false);
+  // scaler.scaleCanvasTo(20);
 
   // createAdaptiveCanvas(0.4, 0.4, false);
   // scaler.scaleCanvasTo(20000);
@@ -10,7 +13,7 @@ function setup() {
   // scaler.scaleCanvasTo(0.5);
 
 
-  // ##### Observações
+  // #####Observações
 
   // Maximum canvas size (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas)
   // The exact maximum size of a <canvas> element depends on the browser and environment. While in most cases the maximum dimensions exceed 10,000 x 10,000 pixels, notably iOS devices limit the canvas size to only 4,096 x 4,096 pixels. See canvas size limits in different browsers and devices (2021).
